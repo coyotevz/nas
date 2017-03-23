3a2a172b9d8811ef4fbc3a80b0af7187cdd6532a
Augusto Roccasalva <augusto@rioplomo.com.ar>
2017-03-23 00:10:33 -0300

    Add accounts relation to bank resource

--- nas/api/bank.py
+++ nas/api/bank.py
@@ -2,7 +2,7 @@
 
 from marshmallow import Schema, fields
 
-from ..tonic import ModelResource
+from ..tonic import ModelResource, Relation
 from ..models import Bank, BankAccount
 
 
@@ -14,22 +14,28 @@ class BankSchema(Schema):
     cuit = fields.String(length=11)
 
 
-class BankResource(ModelResource):
-
-    class Meta:
-        name = 'banks'
-        model = Bank
-        schema = BankSchema
-
-
 class BankAccountSchema(Schema):
 
     id = fields.Integer(dump_only=True)
+    bank_id = fields.Integer(required=True, load_only=True)
+    entity_id = fields.Integer(required=True, load_only=True)
     branch = fields.String()
     acc_type = fields.String()
     number = fields.String()
     owner = fields.String()
     cbu = fields.String()
+
+    bank = fields.Nested(BankSchema, exclude=('accounts'))
+
+
+class BankResource(ModelResource):
+
+    accounts = Relation(schema=BankAccountSchema, exclude=('bank',))
+
+    class Meta:
+        name = 'banks'
+        model = Bank
+        schema = BankSchema
 
 
 class BankAccountResource(ModelResource):
